55209091d1509f1b1bf6fbddf6931d0dfda8a018
Kelly Obrazcova <kelly.obrazcova@gmail.com>
2021-01-07 16:26:26 +0000

    Added HttpPost Create method

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -67,5 +67,15 @@ namespace StarChart.Controllers
             }
             return Ok(celestialObjects);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject celestialObject)
+        {
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
+        }
+
+
     }
 }
