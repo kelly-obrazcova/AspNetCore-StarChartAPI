9e59f0b4d08438cf301ed16281245e3effb6f27d
Kelly Obrazcova <kelly.obrazcova@gmail.com>
2021-01-07 12:34:01 +0000

    Unsuccessful at adding HttpGet

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,21 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("int:id", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (id != _context.CelestialObjects.Id)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                _context.CelestialObjects.Satellite = CelestialObject.OrbitedObjectId;
+                return Ok();
+            }
         }
     }
 }
